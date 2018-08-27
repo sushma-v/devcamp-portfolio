0bd8044ff1528bba91aa868a7942cac4e42e4b2f
Sushma Venugopal <sushma@sushmas-mbp-2.mynet>
2018-08-27 15:02:43 +0100

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+	Topic.create!(title: "Topic #{topic}")
+end
+
+puts "3 topics created"
+
 10.times do |blog|
 	Blog.create!(
 		title: "My Blog Post #{blog}",
-		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		topic_id: Topic.last.id
 	)
 end
 
@@ -16,10 +23,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfilio_item|
+8.times do |portfilio_item|
 	Portfolio.create!(
 		title: "Portfolio title #{portfilio_item}",
-		subtitle: "My great service",
+		subtitle: "Ruby on Rails",
+		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200"
+	)
+end
+
+1.times do |portfilio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfilio_item}",
+		subtitle: "Angular",
 		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
 		main_image: "http://via.placeholder.com/600x400",
 		thumb_image: "http://via.placeholder.com/350x200"
